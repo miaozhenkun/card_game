913066e2ab5866a41688095b7e68d149e9bae73c
miaokun <641105850@qq.com>
2023-08-17 14:28:56 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'dart:io';
+
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
-void main() {
-  final game = FlameGame();
+import 'klondike_game.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.setOrientation(DeviceOrientation.landscapeLeft);
+  await Flame.device.fullScreen();
+  final game = KlondikeGame();
   runApp(GameWidget(game: game));
 }
